0236216dd79f4b834e4b3c0d3437faffe8dc04b3
aacar23 <aacar2023@yahoo.com>
2023-05-05 02:32:18 +0300

    added get_median.c

---
+++ get_median.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+
+int double_cmp(const void *vp1, const void *vp2)
+{
+    double *p1 = (double *)vp1;
+    double *p2 = (double *)vp2;
+    return *p1 > *p2 ? 1 : *p1 < *p2 ? -1 : 0;
+
+}
+
+double get_median_ret(const double *p_sorted, size_t size)
+{
+    if (!(size % 2))
+        return *(p_sorted + size / 2)+ *(p_sorted + size / 2 - 1) / 2.;
+    else
+        return *(p_sorted + size / 2);
+
+}
+
+double get_median(const double *p, size_t size)
+{
+    double *p_temp = malloc(sizeof(double) * size);
+    memcpy(p_temp, p, sizeof(double) * size);
+    qsort(p_temp, size, sizeof(double), &double_cmp);
+    return get_median_ret(p_temp, size);
+}
+
+
+
