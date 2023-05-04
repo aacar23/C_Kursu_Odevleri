a8cdcf6c22a60e3f8f528b5fb09a69eca75cfcc2
Ahmet ACAR <132135056+aacar23@users.noreply.github.com>
2023-05-05 02:55:48 +0300

    Update get_median.c

--- get_median.c
+++ get_median.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 int double_cmp(const void *vp1, const void *vp2)
 {
@@ -11,15 +12,17 @@ int double_cmp(const void *vp1, const void *vp2)
 double get_median_ret(const double *p_sorted, size_t size)
 {
     if (!(size % 2))
-        return *(p_sorted + size / 2)+ *(p_sorted + size / 2 - 1) / 2.;
+        return *(p_sorted + size / 2) + *(p_sorted + size / 2 - 1) / 2.;
     return *(p_sorted + size / 2);
 }
 
 double get_median(const double *p, size_t size)
 {
     double *p_temp = (double *)malloc(sizeof(double) * size);
-    if (!p_temp)
+    if (!p_temp){
+        printf("out of memory");
         exit(EXIT_FAILURE);
+    }
     memcpy(p_temp, p, sizeof(double) * size);
     qsort(p_temp, size, sizeof(double), &double_cmp);
     double ret = get_median_ret(p_temp, size);
