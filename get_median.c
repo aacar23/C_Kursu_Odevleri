4d028293fc2c296566f2a87defb2ebd7ce9d1d8f
Ahmet ACAR <132135056+aacar23@users.noreply.github.com>
2023-05-05 02:49:30 +0300

    Update get_median.c

--- get_median.c
+++ get_median.c
@@ -6,7 +6,6 @@ int double_cmp(const void *vp1, const void *vp2)
     double *p1 = (double *)vp1;
     double *p2 = (double *)vp2;
     return *p1 > *p2 ? 1 : *p1 < *p2 ? -1 : 0;
-
 }
 
 double get_median_ret(const double *p_sorted, size_t size)
@@ -14,7 +13,6 @@ double get_median_ret(const double *p_sorted, size_t size)
     if (!(size % 2))
         return *(p_sorted + size / 2)+ *(p_sorted + size / 2 - 1) / 2.;
     return *(p_sorted + size / 2);
-
 }
 
 double get_median(const double *p, size_t size)
