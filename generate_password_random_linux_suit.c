a02de5ed0706cdebf14c494b4c61d72b4967975e
aacar23 <aacar2023@yahoo.com>
2023-05-06 01:05:19 +0300

    added generate_password_random_linux_suit.c

---
+++ generate_password_random_linux_suit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char *generate_a_password_linux(size_t length_of_password)
+{
+    char *str = calloc(length_of_password + 1, sizeof(char));
+    FILE *f = fopen("/dev/urandom", "r");
+    for (size_t i = 0;i < length_of_password;i++){
+        unsigned int ascii_char;
+        fread(&ascii_char, sizeof(unsigned int), 1, f);
+        *(str + i) = (int)(ascii_char % 94) + 32;
+    }
+    fclose(f);
+    return str;
+}
+
+char **fill_array_with_random_passwords_linux(const char **dest, size_t number_of_elements, size_t length_of_passwords)
+{
+    for (size_t i = 0;i < number_of_elements;i++)
+        *(dest + i) = generate_a_password_linux(length_of_passwords);
+    return (char **)dest;
+}
+
+void free_all_char_pointer_array_elements(char **dest, size_t number_of_elements)
+{
+    while (number_of_elements--)
+        free(*dest++);
+}
