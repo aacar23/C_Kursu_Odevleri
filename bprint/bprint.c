96516216173050c6b2cb9b55e37ba5b9836f0bcc
unknown <aacar2023@yahoo.com>
2023-07-31 15:36:33 +0300

    Update bprint

--- bprint/bprint.c
+++ bprint/bprint.c
@@ -23,7 +23,7 @@ void bprint(int val)
 	memset(str, 48, 32);
 
 	int print_ind = 0;
-	for (int i = 0;i < 32; val /= 2, ++i)
+	for (int i = 0;val; val /= 2, ++i)
 		*(str + i) += val % 2;
 
 	reverse_str(str);
